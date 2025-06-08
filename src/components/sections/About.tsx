c4a9a7eb4590b160c9cda18ab822e633d8ea78cd
Momena-akhtar <momena.akhtar19@gmail.com>
2025-06-08 10:23:59 +0500

    refactored skills section

--- src/components/sections/About.tsx
+++ src/components/sections/About.tsx
@@ -10,7 +10,9 @@ const About = () => {
     "TailwindCSS",
     "Vue",
   ];
-  const backendSkills = ["Express", "Node.js", "MongoDB", "GraphQL", "Langchain"];
+  const backendSkills = ["Express", "Node.js", "MongoDB", "MySQL", "GraphQL"];
+  const tools = ["Git", "CI/CD", "Redis", "Linux", "VS Code", "Postman"];
+  const aiml = ["Python", "Langchain", "LLMs/RAG", "Tensorflow", "OpenAI API", "HuggingFace"];
   return (
     <section
       id="about"
@@ -22,36 +24,7 @@ const About = () => {
           About me.
         </h2>
         <div className="glass rounded-4xl p-6 border-white/10 border hover:-translate-y-1 transition-all ">
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-            <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
-              <h3 className="text-xl font-bold mb-4"> Frontend </h3>
-              <div className="flex flex-wrap gap-2 ">
-                {frontendSkills.map((tech) => (
-                  <span
-                    className="bg-blue-800/50 text-white px-5 py-2 cursor-pointer rounded-full text-sm hover:bg-transparent 
-                        hover:shadow-[0_2px_8px_rgba(59, 130, 246, 0.2)] transition
-                        "
-                  >
-                    {tech}
-                  </span>
-                ))}
-              </div>
-            </div>
-            <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
-              <h3 className="text-xl font-bold mb-4"> Backend </h3>
-              <div className="flex flex-wrap gap-2 ">
-                {backendSkills.map((tech) => (
-                  <span
-                    className="bg-blue-800/50 text-white px-5 py-2 cursor-pointer rounded-full text-sm hover:bg-transparent 
-                        hover:shadow[0_2px_8px_rgba(59, 130, 246, 0.2)] transition
-                        "
-                  >
-                    {tech}
-                  </span>
-                ))}
-              </div>
-            </div>
-            <div>
+        <div className="flex flex-col items-center mb-8">
               <p>
                 <span className="flex items-center gap-2 font-semibold text-lg mb-2">
                   <FontAwesomeIcon icon={faGraduationCap} className="text-blue-800" />
@@ -91,6 +64,65 @@ const About = () => {
                 </a>
               </div>
             </div>
+            <hr className="text-gray-600/30"/>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+            <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
+              <h3 className="text-xl font-bold mb-4"> Frontend </h3>
+              <div className="flex flex-wrap gap-2 ">
+                {frontendSkills.map((tech) => (
+                  <span
+                    className="bg-blue-800/50 text-white px-5 py-2 cursor-pointer rounded-full text-sm hover:bg-transparent 
+                        hover:shadow-[0_2px_8px_rgba(59, 130, 246, 0.2)] transition
+                        "
+                  >
+                    {tech}
+                  </span>
+                ))}
+              </div>
+            </div>
+            <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
+              <h3 className="text-xl font-bold mb-4"> Backend </h3>
+              <div className="flex flex-wrap gap-2 ">
+                {backendSkills.map((tech) => (
+                  <span
+                    className="bg-blue-800/50 text-white px-5 py-2 cursor-pointer rounded-full text-sm hover:bg-transparent 
+                        hover:shadow[0_2px_8px_rgba(59, 130, 246, 0.2)] transition
+                        "
+                  >
+                    {tech}
+                  </span>
+                ))}
+              </div>
+            </div>
+            <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
+              <h3 className="text-xl font-bold mb-4"> AI/ML </h3>
+              <div className="flex flex-wrap gap-2 ">
+                {aiml.map((tech) => (
+                  <span
+                    className="bg-blue-800/50 text-white px-5 py-2 cursor-pointer rounded-full text-sm hover:bg-transparent 
+                        hover:shadow[0_2px_8px_rgba(59, 130, 246, 0.2)] transition
+                        "
+                  >
+                    {tech}
+                  </span>
+                ))}
+              </div>
+            </div>
+            <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
+              <h3 className="text-xl font-bold mb-4"> Tools </h3>
+              <div className="flex flex-wrap gap-2 ">
+                {tools.map((tech) => (
+                  <span
+                    className="bg-blue-800/50 text-white px-5 py-2 cursor-pointer rounded-full text-sm hover:bg-transparent 
+                        hover:shadow[0_2px_8px_rgba(59, 130, 246, 0.2)] transition
+                        "
+                  >
+                    {tech}
+                  </span>
+                ))}
+              </div>
+            </div>
+            
           </div>
         </div>
        
