367af97b417e7564c185f52a0eca8d00c021222d
Momena <momena.akhtar19@gmail.com>
2025-08-14 19:20:14 +0500

    add reality cheque project

--- src/components/sections/Projects.tsx
+++ src/components/sections/Projects.tsx
@@ -14,21 +14,22 @@ const Projects = () => {
           Featured projects.
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-            {/* SYNC */}
+          {/* REALITY CHEQUE */}
           <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-800/50
           hover:shadow-[0_2px_8px_rgba(59, 130, 246, 0.2)] transition cursor-pointer">
-            <h3 className="text-xl font-bold mb-2"> Sync </h3>
+            <h3 className="text-xl font-bold mb-2"> Reality Cheque </h3>
             <p className="text-gray-400 mb-4 ">
               {" "}
-              A powerful real-time collaborative whiteboard built with the MERN stack, enabling teamwork, instant updates, and interactive brainstorming from anywhere.
+              An easy-to-use site for agencies and freelancers offering a variety of custom GPT tools, organized into 10 main categories with several helpful technical tools.
             </p>
             <div className="m-2 flex flex-wrap gap-2">
               {[
-                "MERN",
-                "Typescript",
+                "NextJS",
+                "Express",
+                "MongoDB",
                 "TailwindCSS",
-                "Firebase",
-                "Socket.io",
+                "Langchain",
+                "Stripe",
               ].map((tech, key) => (
                 <span
                   key={key}
@@ -42,18 +43,27 @@ const Projects = () => {
             </div>
             <a 
             target='_blank'
-            href="https://github.com/Momena-akhtar/sync-board"
+            href="https://github.com/Momena-akhtar/reality-cheque"
             className=" text-white hover:text-blue-800 text-sm transition-colors">
                 <FontAwesomeIcon icon={faGithub}  className="text-white pr-2" />
                 View Project 
                 <span className="ml-1 text-sm">&#8594;</span>
+            </a>
+            <a 
+              target='_blank'
+              href="https://ai.realitycheque.co/"
+              className="text-white hover:text-blue-800 text-sm transition-colors block"
+            >
+              <FontAwesomeIcon icon={faLink} className="text-white pr-2" />
+              Live Project 
+              <span className="ml-1 text-sm">&#8594;</span>
             </a>
             <div className='ml-1 text-sm text-gray-400'>
                 Collaborators: 
                 <a 
                 target='_blank'
                 className='text-blue-800'
-                href="https://github.com/12Danish"> {" "}Danish Abbas</a>
+                href="/"> {" "}Shaheer-Ul-Haq</a>
             </div>
           </div>
           {/* QUIZ RUSH */}
@@ -154,6 +164,48 @@ const Projects = () => {
               Live Project 
               <span className="ml-1 text-sm">&#8594;</span>
             </a>
+          </div>
+           {/* SYNC */}
+          <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-800/50
+          hover:shadow-[0_2px_8px_rgba(59, 130, 246, 0.2)] transition cursor-pointer">
+            <h3 className="text-xl font-bold mb-2"> Sync </h3>
+            <p className="text-gray-400 mb-4 ">
+              {" "}
+              A powerful real-time collaborative whiteboard built with the MERN stack, enabling teamwork, instant updates, and interactive brainstorming from anywhere.
+            </p>
+            <div className="m-2 flex flex-wrap gap-2">
+              {[
+                "MERN",
+                "Typescript",
+                "TailwindCSS",
+                "Firebase",
+                "Socket.io",
+              ].map((tech, key) => (
+                <span
+                  key={key}
+                  className="bg-blue-800/50 text-white px-5 py-2 cursor-pointer rounded-full text-sm hover:bg-transparent 
+                        hover:shadow[0_2px_8px_rgba(59, 130, 246, 0.2)] transition
+                        "
+                >
+                  {tech}
+                </span>
+              ))}
+            </div>
+            <a 
+            target='_blank'
+            href="https://github.com/Momena-akhtar/sync-board"
+            className=" text-white hover:text-blue-800 text-sm transition-colors">
+                <FontAwesomeIcon icon={faGithub}  className="text-white pr-2" />
+                View Project 
+                <span className="ml-1 text-sm">&#8594;</span>
+            </a>
+            <div className='ml-1 text-sm text-gray-400'>
+                Collaborators: 
+                <a 
+                target='_blank'
+                className='text-blue-800'
+                href="https://github.com/12Danish"> {" "}Danish Abbas</a>
+            </div>
           </div>
         </div>
       </div>
