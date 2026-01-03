648bc5f795015077ab0bc7a9cc31d72bd6ec4490
Momena <momena.akhtar19@gmail.com>
2026-01-03 17:59:50 +0500

    remove loading screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import "./App.css";
-import LoadingScreen from "./components/LoadingScreen";
 import Navbar from "./components/Navbar";
 import MobileMenu from "./components/MobileMenu";
 import Home from "./components/sections/Home";
@@ -11,12 +10,10 @@ import Contact from "./components/sections/Contact";
 import Experiences from "./components/sections/Experiences";
 import Footer from "./components/sections/Footer";
 const App = () => {
-  const [isLoaded, setLoaded] = useState(false);
   const [menuOpen, setMenuOpen] = useState(false);
 
-  return <>{!isLoaded && <LoadingScreen  onComplete = {()=> setLoaded(true)}/>}{" "}
-  
-  <div className={`min-h-screen transition-opacity duration-700 ${isLoaded ? "opacity-100": "opacity-0"} bg-black text-gray-100 ` }>
+  return <>
+  <div className={`min-h-screen transition-opacity duration-700 bg-black text-gray-100 ` }>
   <Navbar menuOpen = { menuOpen } setMenuOpen = { setMenuOpen }/>
   <MobileMenu menuOpen = { menuOpen } setMenuOpen = { setMenuOpen }/>
   <Home />
